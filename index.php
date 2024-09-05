97c2395cd0119a576be3553eba28c7418491501a
Nabsatang02 <Nabtang@siamkane.com>
2024-09-05 19:05:42 +0700

    Added Logo And Search Panel Section

---
+++ index.php
@@ -0,0 +1,41 @@
+<!---------- Meta HTML Starts --------->
+<html>
+   <head>
+      <title>Website Site</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="css/style.css">
+   </head>
+   <body>
+<!---------- Meta HTML Ends --------->
+
+<!---------- Logo and Search Panel HTML Code Starts --------->
+<div class="width-100 search-panel">
+  <div class="container">
+    <div class="width-20">
+      <img src="images/logo.png" class="logo">
+    </div>
+    <div class="width-50">
+      <input class="search-textbox" type="text" Placeholder="Search for products, brand and more">
+      <button class="search-button">
+        <i class="fa fa-search" aria-hidden="true"></i>
+      </button>
+    </div>
+    <div class="width-30">
+      <ul class="cart-sect">
+        <li>
+          <i class="fa fa-heart-o" aria-hidden="true"></i>
+          <a class="head1mr" href="#">Whislist</a>
+        </li>
+        <li>
+          <i class="fa fa-user-circle-o" aria-hidden="true"></i>
+          <a href="#">Login & Signup</a>
+        </li>
+        <li>
+          <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+          <a href="#">Cart (0)</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+<!---------- Logo and Search Panel HTML Code Ends --------->
